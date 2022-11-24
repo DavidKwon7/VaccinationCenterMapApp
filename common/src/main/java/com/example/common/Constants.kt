36e1d639ef33be2173780f4d3ae355a3f7899db1
DavidKwon7 <khjunejune10@gmail.com>
2022-11-25 03:23:15 +0900

    [FEAT] VaccinationCenterAPI 코드 작성

--- common/src/main/java/com/example/common/Constants.kt
+++ common/src/main/java/com/example/common/Constants.kt
@@ -3,7 +3,7 @@ package com.example.common
 class Constants {
 
     companion object {
-        const val API_KEY = "bNmSjmL3NWL%2FmAmsQV0SyDT%2B8DCdZckhVg5%2FtSsmJHa47eBZBE%2BaFvCHYxeM1Dsz2FcgQ64elqYL3mr6GUyjOg%3D%3D"
+        const val SERVICE_KEY = "bNmSjmL3NWL%2FmAmsQV0SyDT%2B8DCdZckhVg5%2FtSsmJHa47eBZBE%2BaFvCHYxeM1Dsz2FcgQ64elqYL3mr6GUyjOg%3D%3D"
         const val BASE_URL = "https://api.odcloud.kr/api"
         const val PAGING_START_PAGE = 1
     }
---
+++ data/src/main/java/com/example/data/remote/VaccinationCenterAPI.kt
@@ -0,0 +1,17 @@
+package com.example.data.remote
+
+import com.example.common.Constants.Companion.SERVICE_KEY
+import com.example.data.model.VaccinationCenterResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface VaccinationCenterAPI {
+
+    @GET("/15077586/v1/centers")
+    suspend fun getVaccinationCenter(
+        @Query("page") page: Int,
+        @Query("perPage") perPage: Int,
+        @Query("serviceKey") serviceKey: String = SERVICE_KEY
+    ): VaccinationCenterResponse
+
+}
