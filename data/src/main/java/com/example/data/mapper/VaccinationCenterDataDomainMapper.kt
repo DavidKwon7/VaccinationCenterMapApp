8846c0a02d6742caeec35781b75906d421b37686
DavidKwon7 <khjunejune10@gmail.com>
2022-11-25 04:00:15 +0900

    [FEAT] VaccinationCenterDataDomainMapper

---
+++ data/src/main/java/com/example/data/mapper/VaccinationCenterDataDomainMapper.kt
@@ -0,0 +1,48 @@
+package com.example.data.mapper
+
+import com.example.common.Mapper
+import com.example.data.model.VaccinationCenterResponse
+import com.example.domain.entity.VaccinationCenterEntityModel
+import javax.inject.Inject
+
+class VaccinationCenterDataDomainMapper @Inject constructor() :
+    Mapper<VaccinationCenterResponse, VaccinationCenterEntityModel> {
+
+    override fun from(i: VaccinationCenterResponse?): VaccinationCenterEntityModel {
+        return VaccinationCenterEntityModel(
+            address = i?.address,
+            centerName = i?.centerName,
+            centerType = i?.centerType,
+            createdAt = i?.createdAt,
+            facilityName = i?.facilityName,
+            id = i?.id,
+            lat = i?.lat,
+            lng = i?.lng,
+            org = i?.org,
+            phoneNumber = i?.phoneNumber,
+            sido = i?.sido,
+            sigungu = i?.sigungu,
+            updatedAt = i?.updatedAt,
+            zipCode = i?.zipCode
+        )
+    }
+
+    override fun to(o: VaccinationCenterEntityModel?): VaccinationCenterResponse {
+        return VaccinationCenterResponse(
+            address = o?.address,
+            centerName = o?.centerName,
+            centerType = o?.centerType,
+            createdAt = o?.createdAt,
+            facilityName = o?.facilityName,
+            id = o?.id,
+            lat = o?.lat,
+            lng = o?.lng,
+            org = o?.org,
+            phoneNumber = o?.phoneNumber,
+            sido = o?.sido,
+            sigungu = o?.sigungu,
+            updatedAt = o?.updatedAt,
+            zipCode = o?.zipCode
+        )
+    }
+}
