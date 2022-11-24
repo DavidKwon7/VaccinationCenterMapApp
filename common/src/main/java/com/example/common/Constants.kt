b2c49e844839d61ef3147827a3060b8bfd961b4c
DavidKwon7 <khjunejune10@gmail.com>
2022-11-25 03:08:47 +0900

    [CHORE] API_KEY & BASE_URL 등록

--- common/src/main/java/com/example/common/Constants.kt
+++ common/src/main/java/com/example/common/Constants.kt
@@ -3,6 +3,8 @@ package com.example.common
 class Constants {
 
     companion object {
-
+        const val API_KEY = "bNmSjmL3NWL%2FmAmsQV0SyDT%2B8DCdZckhVg5%2FtSsmJHa47eBZBE%2BaFvCHYxeM1Dsz2FcgQ64elqYL3mr6GUyjOg%3D%3D"
+        const val BASE_URL = "https://api.odcloud.kr/api"
+        const val PAGING_START_PAGE = 1
     }
 }
