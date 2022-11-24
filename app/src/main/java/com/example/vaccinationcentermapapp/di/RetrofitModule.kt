c92259d2fc5cb8ba1c12bd6a16c968f48979d0c8
DavidKwon7 <khjunejune10@gmail.com>
2022-11-25 03:27:29 +0900

    [FEAT] RetrofitModule 코드 작성

---
+++ app/src/main/java/com/example/vaccinationcentermapapp/di/RetrofitModule.kt
@@ -0,0 +1,63 @@
+package com.example.vaccinationcentermapapp.di
+
+import android.app.Application
+import com.example.common.Constants.Companion.BASE_URL
+import com.example.data.remote.VaccinationCenterAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    private const val CONNECT_TIMEOUT = 15L
+    private const val WRITE_TIMEOUT = 15L
+    private const val READ_TIMEOUT = 15L
+
+    @Provides
+    @Singleton
+    fun provideCache(application: Application): Cache {
+        return Cache(application.cacheDir, 10L * 1024 * 1024)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(cache: Cache): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            cache(cache)
+            connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            retryOnConnectionFailure(true)
+            addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+        }.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideVaccinationCenterService(retrofit: Retrofit): VaccinationCenterAPI {
+        return retrofit.create(VaccinationCenterAPI::class.java)
+    }
+}
