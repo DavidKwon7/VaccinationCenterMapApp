a562467fc22b78dc2df5ead416370adaeefeacfe
DavidKwon7 <khjunejune10@gmail.com>
2022-11-25 14:58:33 +0900

    [FEAT] fragment 적용

--- app/src/main/java/com/example/vaccinationcentermapapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/vaccinationcentermapapp/ui/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.example.presentation.vm.SplashState
 import com.example.presentation.vm.SplashViewModel
 import com.example.vaccinationcentermapapp.R
+import com.example.vaccinationcentermapapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -15,40 +17,12 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    val splashViewModel: SplashViewModel by viewModels()
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //getData()
-        observeData()
-        for (i: Int in 1..10) {
-            getData(i)
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
     }
-
-    private fun getData(page:Int) {
-        splashViewModel.getVaccinationCenter(page)
-    }
-
-    private fun observeData() {
-        lifecycleScope.launch {
-            splashViewModel.getVaccinationCenter.collect {state ->
-                when(state) {
-                    is SplashState.Loading -> {
-
-                    }
-                    is SplashState.Success -> {
-                        val data = state.data
-                        val tv = findViewById<TextView>(R.id.tv)
-                        tv.text = data.toString()
-                    }
-                    is SplashState.Failed -> {
-
-                    }
-                }
-            }
-        }
-    }
-
 }
---
+++ app/src/main/java/com/example/vaccinationcentermapapp/ui/splash/SplashFragment.kt
@@ -0,0 +1,71 @@
+package com.example.vaccinationcentermapapp.ui.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.presentation.vm.SplashState
+import com.example.presentation.vm.SplashViewModel
+import com.example.vaccinationcentermapapp.R
+import com.example.vaccinationcentermapapp.databinding.FragmentSplashBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SplashFragment : Fragment() {
+
+    lateinit var binding: FragmentSplashBinding
+    private val splashViewModel: SplashViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_splash, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        for (i: Int in 1..10) {
+            getVaccinationCenter(i)
+        }
+
+        observeData()
+    }
+
+    private fun getVaccinationCenter(page:Int) {
+        splashViewModel.getVaccinationCenter(page)
+    }
+
+    private fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                splashViewModel.getVaccinationCenter.collect {state ->
+                    when(state) {
+                        is SplashState.Loading -> {
+
+                        }
+                        is SplashState.Success -> {
+                            val data = state.data
+                            binding.tv.text = data.toString()
+                        }
+                        is SplashState.Failed -> {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
