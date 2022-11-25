fc28e478e795e25a01888a462f14a2e3372d2b02
DavidKwon7 <khjunejune10@gmail.com>
2022-11-25 11:42:48 +0900

    [FEAT] vaccinationCenterAPI 화면에 띄우기

--- app/src/main/java/com/example/vaccinationcentermapapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/vaccinationcentermapapp/ui/MainActivity.kt
@@ -1,14 +1,51 @@
 package com.example.vaccinationcentermapapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.presentation.vm.SplashState
+import com.example.presentation.vm.SplashViewModel
 import com.example.vaccinationcentermapapp.R
-import timber.log.Timber
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    val splashViewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+        getData()
+        observeData()
     }
+
+    private fun getData() {
+        splashViewModel.getVaccinationCenter()
+    }
+
+    private fun observeData() {
+        lifecycleScope.launch {
+            splashViewModel.getVaccinationCenter.collect {state ->
+                when(state) {
+                    is SplashState.Loading -> {
+
+                    }
+                    is SplashState.Success -> {
+                        val data = state.data
+                        val tv = findViewById<TextView>(R.id.tv)
+                        tv.text = data.toString()
+                    }
+                    is SplashState.Failed -> {
+
+                    }
+                }
+            }
+        }
+    }
+
 }
