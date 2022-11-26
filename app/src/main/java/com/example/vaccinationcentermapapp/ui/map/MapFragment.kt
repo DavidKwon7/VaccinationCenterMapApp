31efd74f0bf99b83f8260241136ebffae5db583d
DavidKwon7 <khjunejune10@gmail.com>
2022-11-26 12:26:02 +0900

    [FEAT] 받아온 데이터로 마커 띄우기

--- app/src/main/java/com/example/vaccinationcentermapapp/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/vaccinationcentermapapp/ui/map/MapFragment.kt
@@ -26,6 +26,10 @@ import com.naver.maps.map.overlay.OverlayImage
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
+// todo 1 마커 여러개 띄우기 -> 마커 관련 다양한 설정 custom
+// todo 2  lifeCycle - mapView 수정
+
+
 @AndroidEntryPoint
 class MapFragment : Fragment() {
 
@@ -34,6 +38,9 @@ class MapFragment : Fragment() {
 
     private var naverMap: NaverMap? = null
     private var mapView: MapView? = null
+
+    // 이렇게 전역 변수를 선언하는 것 말고는 방법이 없을까?
+    private lateinit var location: Location
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,14 +61,16 @@ class MapFragment : Fragment() {
         mapView?.onCreate(savedInstanceState)   // why?
         mapView?.getMapAsync(callback)
 
-        makeMarker()
+
     }
 
 
     private fun makeMarker() {
         val marker = Marker()
-        marker.position = LatLng(37.5670135, 126.9783740)
-        marker.icon = OverlayImage.fromResource(R.drawable.ic_launcher_foreground)
+        //marker.position = LatLng(37.5670135, 126.9783740)
+        Log.d("Location Log", "makeMarker: ${location.lat}, ${location.lng}")
+        marker.position = LatLng(location.lat, location.lng)
+        //marker.icon = OverlayImage.fromResource(R.drawable.ic_launcher_foreground)
         marker.map = naverMap
     }
 
@@ -106,7 +115,8 @@ class MapFragment : Fragment() {
 
                             state.data.forEach {
                                 Log.d("TAG1", "observeData: ${it.lng}")
-                                createMarker(it.lat, it.lng)
+                                //createMarker(it.lat, it.lng)
+                                location = Location(it.lat!!.toDouble(), it.lng!!.toDouble())   // !! 안 쓰려고 했지만 일단 어쩔 수 없네..
                             }
                         }
                         is MapState.Failed -> {
@@ -117,12 +127,9 @@ class MapFragment : Fragment() {
             }
         }
     }
-
-    private fun createMarker(lat: String?, lng: String?) {
-        /*val marker = Marker()
-        if (lat != null && lng != null) {
-            marker.position = LatLng(lat.toDouble(), lng.toDouble())
-        }
-        marker.map = naverMap*/
-    }
 }
+
+data class Location(
+    val lat: Double,
+    val lng: Double
+)
