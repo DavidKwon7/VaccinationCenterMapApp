ef6b4911d313477d55f768bcdf8f91345b4b1d3a
DavidKwon7 <khjunejune10@gmail.com>
2022-11-27 01:39:56 +0900

    [FEAT] 마커 여러개 띄우기 구현

--- app/src/main/java/com/example/vaccinationcentermapapp/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/vaccinationcentermapapp/ui/map/MapFragment.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
+import com.example.presentation.model.VaccinationCenterUiModel
 import com.example.presentation.vm.MapState
 import com.example.presentation.vm.MapViewModel
 import com.example.vaccinationcentermapapp.R
@@ -26,7 +27,7 @@ import com.naver.maps.map.overlay.OverlayImage
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
-// todo 1 마커 여러개 띄우기 -> 마커 관련 다양한 설정 custom
+// todo 1 마커 여러개 띄우기(리스트를 만들어 데이터 저장해줘야 할듯) -> 마커 관련 다양한 설정 custom
 // todo 2  lifeCycle - mapView 수정
 
 
@@ -41,6 +42,8 @@ class MapFragment : Fragment() {
 
     // 이렇게 전역 변수를 선언하는 것 말고는 방법이 없을까?
     private lateinit var location: Location
+
+    private lateinit var markerData: List<VaccinationCenterUiModel> // 추후에 삭제 !
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -64,14 +67,31 @@ class MapFragment : Fragment() {
 
     }
 
-
+    // marker 를 mutableList에 넣어줘서 마커 여러개를 생성..?
+    // https://keoroo.tistory.com/42 참조하자
     private fun makeMarker() {
         val marker = Marker()
         //marker.position = LatLng(37.5670135, 126.9783740)
         Log.d("Location Log", "makeMarker: ${location.lat}, ${location.lng}")
-        marker.position = LatLng(location.lat, location.lng)
+        //marker.position = LatLng(location.lat, location.lng)
+        markerData.forEach {
+            marker.position = LatLng(it.lat!!.toDouble(), it.lng!!.toDouble())
+            Log.d("TAG", "makeMarker: ${it.lat}")   // 로그 lat 여러개 찍히는 것 확인
+        }
         //marker.icon = OverlayImage.fromResource(R.drawable.ic_launcher_foreground)
         marker.map = naverMap
+    }
+
+    private fun addMarker() {
+        val markers = mutableListOf<Marker>()
+        markerData.forEach {
+            markers += Marker().apply {
+                position = LatLng(it.lat!!.toDouble(), it.lng!!.toDouble())
+            }
+            markers.forEach { marker ->
+                marker.map = naverMap
+            }
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -89,7 +109,8 @@ class MapFragment : Fragment() {
         override fun onMapReady(naverMap: NaverMap) {
             this@MapFragment.naverMap = naverMap
 
-            makeMarker()
+            //makeMarker()
+            addMarker()
         }
     }
 
@@ -117,6 +138,7 @@ class MapFragment : Fragment() {
                                 Log.d("TAG1", "observeData: ${it.lng}")
                                 //createMarker(it.lat, it.lng)
                                 location = Location(it.lat!!.toDouble(), it.lng!!.toDouble())   // !! 안 쓰려고 했지만 일단 어쩔 수 없네..
+                                markerData = state.data
                             }
                         }
                         is MapState.Failed -> {
